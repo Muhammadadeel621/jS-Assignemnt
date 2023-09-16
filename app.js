91336fd931c617feb4fa82d63004819d1924bd88
tooba889 <toobatooba891@gmail.com>
2023-09-16 20:03:02 +0500

    in progress

---
+++ app.js
@@ -0,0 +1,183 @@
+//                     Chapter 1 (Alerts)
+
+// alert("first name is : Muahmmad");
+// alert("last name is : Adeel");
+// alert("Email is : raoadeel960@gmail.com");
+// alert("Phone Number is : 03332505647");
+// alert("Password is : Adeel123@");
+
+// alert("You're learning JavaScript!");
+
+// alert("Pakistan will win the upcoming 50-overs Worldcup");
+
+//                     Chapter 2 (Variables for Strings)
+
+// var studentAge;
+
+// var studentName;
+// studentName = "Muhammad Adeel";
+// console.log(studentName);
+
+// var teamName = "Parhaku Gang";
+// alert("Team Name is : " + teamName);
+
+// var bestMan = "Charlie";
+// bestMan = "Ruhail";
+// alert(bestMan);
+
+//                     Chapter 2 (Variables for Numbers)
+
+// var caseQty;
+// caseQty = 144;
+
+// var num = 9;
+
+// var sumOfTwoNumbers;
+// sumOfTwoNumbers = 6 + 9;
+// alert("Sum of two Numbers is : " + sumOfTwoNumbers);
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+
+// var num = 5;
+// num = num + 7;
+// alert(num);
+
+//             Chapter 4 (Variable names Legal and Illegal)
+
+// var productCost = 3.45;
+
+// var nameOfBand;
+
+// var employeeId = 568;
+
+// var muhammadAdeel ;
+
+// legal
+
+// var number = 5;
+// var studentAge = 15;
+// var student_age = 45;
+// var number1 = 45 ;
+
+// illegal
+
+// var rahul age = 56 ;
+// var 1age = 45;
+// var @name = "adeel";
+// var name@ = "Adeel";
+
+//                Chapter 5 (Math Expression I)
+
+//    name is "Modulo Operator" and symbol is %
+
+// var num = 20 % 6;
+// alert(num);
+
+// var largeNumber = 1000 * 2000;
+
+// var variable1 = 10;
+// var variable2 = 5;
+// var result = variable1 - variable2;
+// console.log(result);
+
+// var variable1 = 13;
+// var variable2 = 5;
+// var remainingResult = variable1 % variable2;
+// console.log(remainingResult);
+
+// var num = +prompt("Enter the number : ");
+// var multipleOfTwo = 2 * num;
+// alert(multipleOfTwo);
+
+//            Chapter 6 (Math Expression II)
+
+// var x = 5;
+// alert(++x);
+
+// var x = 100;
+// alert(--x);
+
+// var x = 50;
+// var y = x++;
+// alert(y);
+
+// var y = 50;
+// var z = --y;
+// alert(z);
+
+// var number = 56;
+// var newNum = --number;
+// alert(newNum);
+
+// var number = 56;
+// var newNum = ++number;
+
+// var number = 56;
+// var newNum = ++number;
+// alert(newNum);
+
+//             Chapter 7 (Math Expression III)
+
+// var calculatedNum = 2 + 2 * 6;
+// alert(calculatedNum);
+
+// calculatedNum = (2 + 2) * 6;
+// alert(calculatedNum);
+
+// var calculatedNum = (2 + 2) * (4 + 2);
+// alert(calculatedNum);
+
+// var calculatedNum = (2 + 2) * 4 + 2;
+// alert(calculatedNum);
+
+// var costAnswer = (2 + 2) * (4 + 10);
+// alert(costAnswer);
+
+// var unitsAnswer = 2 + (2 * 4) + 10;
+// alert(unitsAnswer);
+
+// var pressureResult = (4 / 2) * 4;
+// alert(pressureResult);
+
+//            Chapter 8 (Concatenating Text Strings)
+
+// var num = "2" + "2";
+// alert(num);
+
+// var message = "Hello " + " Dolly";
+// alert(message);
+
+// alert("33" + 3);
+//   333
+
+// alert("Pakistan " + " Zindabad");
+
+// var studentName = "Adeel";
+// var age = 12;
+// alert(studentName + age);
+
+// var fName = "Muhammad";
+// var lName = " hasan";
+// var fullName = fName + lName;
+// alert(fullName);
+
+//                 Chapter 9 (Prompts)
+
+// var firstName = prompt("Enter the first name : ");
+// alert(firstName);
+
+// var country = prompt("Enter your Country : ", "china");
+// alert(country);
+
+// var yourName = prompt("Enter Your Name");
+
+// var grade = prompt("enter your grade : ", "passed or failed");
+
+// var message = "Enter your name:";
+// var defaultResponse = "John";
+// var userResponse = prompt(message, defaultResponse);
+
+// alert(userResponse);
